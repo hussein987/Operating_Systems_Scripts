27bb2d4f993d4f309aad950da63946f267502267
hussein987 <hussein_younes2015@hotmail.com>
2019-08-29 23:12:54 +0300

    week2

--- week2/ex3.c
+++ week2/ex3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(){
-
-	int n;
-	scanf("%d", &n);
+int main(int argc, char *argv[]){
+	int n = atoi(argv[1]);
 	for(int i=1;i<=n;i++){
 		for(int j=0;j<n-i;j++)
 			printf(" ");
